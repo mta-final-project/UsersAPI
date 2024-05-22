7b2a7eeb69dc27efc2089262649f74a9f97e69af
Tal Alon <talalon369@gmail.com>
2024-05-22 14:57:59 +0300

    handle boto client error

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,6 +1,7 @@
 from typing import Annotated, Any
 
-from fastapi import APIRouter, status, Depends
+from botocore.exceptions import ClientError
+from fastapi import APIRouter, status, Depends, HTTPException
 
 from src.api.schemas import LoginSchema, ConfirmEmailSchema, CreateUserSchema
 from src.api.service import CognitoService
@@ -16,14 +17,32 @@ ServiceDep = Annotated[
 
 @router.post("/register", status_code=status.HTTP_200_OK)
 async def register(params: CreateUserSchema, service: ServiceDep) -> Any:
-    return service.register(params)
+    try:
+        return service.register(params)
+    except ClientError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=e.response["Error"],
+        )
 
 
 @router.post("/confirm-email", status_code=status.HTTP_200_OK)
 async def confirm_email(params: ConfirmEmailSchema, service: ServiceDep) -> Any:
-    return service.confirm_email(params)
+    try:
+        return service.confirm_email(params)
+    except ClientError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=e.response["Error"],
+        )
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login(params: LoginSchema, service: ServiceDep) -> Any:
-    return service.login(params)
+    try:
+        return service.login(params)
+    except ClientError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=e.response["Error"],
+        )
