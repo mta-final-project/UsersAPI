0dcfbfc84b9540026499502eced13cd0299caa82
Tal Alon <talalon369@gmail.com>
2024-05-22 17:55:02 +0300

    move all boto3 error handling into a single exception handler

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,7 +1,6 @@
 from typing import Annotated, Any
 
-from botocore.exceptions import ClientError
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, status, Depends
 
 from src.api.schemas import LoginSchema, ConfirmEmailSchema, CreateUserSchema
 from src.api.service import CognitoService
@@ -17,32 +16,14 @@ ServiceDep = Annotated[
 
 @router.post("/register", status_code=status.HTTP_200_OK)
 async def register(params: CreateUserSchema, service: ServiceDep) -> Any:
-    try:
-        return service.register(params)
-    except ClientError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=e.response["Error"],
-        )
+    return service.register(params)
 
 
 @router.post("/confirm-email", status_code=status.HTTP_200_OK)
 async def confirm_email(params: ConfirmEmailSchema, service: ServiceDep) -> Any:
-    try:
-        return service.confirm_email(params)
-    except ClientError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=e.response["Error"],
-        )
+    return service.confirm_email(params)
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login(params: LoginSchema, service: ServiceDep) -> Any:
-    try:
-        return service.login(params)
-    except ClientError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=e.response["Error"],
-        )
+    return service.login(params)
--- src/core/app.py
+++ src/core/app.py
@@ -1,11 +1,16 @@
+from botocore.exceptions import ClientError
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.core.error_hanlers import handle_boto_error
 from src.api.endpoints import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+app.add_exception_handler(ClientError, handler=handle_boto_error)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
